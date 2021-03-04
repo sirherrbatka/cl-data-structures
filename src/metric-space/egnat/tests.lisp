7859352255948fac4a9427e7dcf1ce75f73da22b
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-03-04 21:58:22 +0100

    Corrected test for the egnat.

--- src/metric-space/egnat/tests.lisp
+++ src/metric-space/egnat/tests.lisp
@@ -5,6 +5,31 @@
 
 (plan 45)
 
+(defun levenshtein (str1 str2)
+  (check-type str1 string)
+  (check-type str2 string)
+  (let ((n (length str1))
+        (m (length str2)))
+    (cond ((= 0 n) (return-from levenshtein m))
+          ((= 0 m) (return-from levenshtein n)))
+    (let ((col (make-array (1+ m) :element-type 'fixnum))
+          (prev-col (make-array (1+ m) :element-type 'fixnum)))
+      (iterate
+        (for i from 0 below (1+ m))
+        (setf (aref prev-col i) i))
+      (iterate
+        (for i below n)
+        (setf (aref col 0) (1+ i))
+        (iterate
+          (for j below m)
+          (setf (aref col (1+ j))
+                (min (1+ (aref col j))
+                     (1+ (aref prev-col (1+ j)))
+                     (+ (aref prev-col j)
+                        (if (char-equal (aref str1 i) (aref str2 j)) 0 1)))))
+        (rotatef col prev-col))
+      (aref prev-col m))))
+
 (let* ((path (asdf:system-relative-pathname :cl-data-structures "test/files/words.txt"))
        (data (serapeum:vect))
        (count 0))
@@ -18,7 +43,7 @@
                'cl-ds.ms.egnat:mutable-egnat-metric-set
                (lambda (a b)
                  (incf count)
-                 (cl-ds.utils.metric:levenshtein-metric a b))
+                 (levenshtein a b))
                'non-negative-fixnum
                :branching-factor 50
                :parallel t
@@ -32,7 +57,7 @@
       (with near = (cl-ds:near set "rose" 1))
       (for n = (cl-ds:consume-front near))
       (while n)
-      (for distance = (cl-ds.utils.metric:levenshtein-metric n "rose"))
+      (for distance = (levenshtein n "rose"))
       (ok (<= distance 1))
       (count t into result)
       (finally (is result 40)))
