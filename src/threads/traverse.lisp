75805b8eff079096e688add9780f0a24cac835fb
Marek Kochanowicz <sirherrbatka@gmail.com>
2025-03-15 09:52:09 +0100

    corrected keyword names

--- src/threads/traverse.lisp
+++ src/threads/traverse.lisp
@@ -1,20 +1,20 @@
 (cl:in-package #:cl-data-structures.threads)
 
 
-(defun traverse (object function &key (batch-size 64) (queue-size 128))
+(defun traverse (object function &key (chunk-size 64) (maximum-queue-size 128))
   (bind ((tasks (make-instance 'task-queue
-                               :queue-size queue-size
+                               :queue-size maximum-queue-size
                                :callback function
-                               :batch-size batch-size)))
+                               :batch-size chunk-size)))
     (cl-ds:traverse object (lambda (x) (task-queue-push tasks x)))
     (task-queue-finalize tasks)
     object))
 
-(defun across (object function &key (batch-size 64) (queue-size 128))
+(defun across (object function &key (chunk-size 64) (maximum-queue-size 128))
   (bind ((tasks (make-instance 'task-queue
-                               :queue-size queue-size
+                               :queue-size maximum-queue-size
                                :callback function
-                               :batch-size batch-size)))
+                               :batch-size chunk-size)))
     (cl-ds:across object (lambda (x) (task-queue-push tasks x)))
     (task-queue-finalize tasks)
     object))
