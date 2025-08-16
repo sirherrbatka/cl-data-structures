c305927296e33d2031ceb567c0581d99549ddcc8
Marek Kochanowicz <marek-kochanowicz@posteo.com>
2025-08-16 14:37:08 +0200

    added test

--- src/dicts/skip-list/tests.lisp
+++ src/dicts/skip-list/tests.lisp
@@ -21,4 +21,12 @@
             :test #'equal)
   (prove:is (cl-ds:lower-bound dict 3) 3))
 
+(let ((dict (cl-ds:make-from-traversable
+             (mapcar (lambda (x) (cons x x)) '(147865 138799 129983 119199 102141 11920))
+             'mutable-skip-list-dictionary
+             #'> #'=)))
+  (prove:is (cl-ds:size dict) 6)
+  (prove:is (cl-ds.alg:count-elements (cl-ds:between* dict :low 102141 :high 0))
+            2))
+
 (prove:finalize)
